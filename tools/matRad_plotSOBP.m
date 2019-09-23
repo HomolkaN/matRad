c3390f674f8890dd63db99a1f62450f637917ac8
homolka <n.homolka@dkfz.de>
2019-09-23 11:42:36 +0200

    Added header to 2 tool files and removed an unnecessary file

--- tools/matRad_plotSOBP.m
+++ tools/matRad_plotSOBP.m
@@ -1,4 +1,31 @@
 function matRad_plotSOBP(ct, stf, cube1, cube2)
+% matRad SOBP plotting function
+%
+% call
+%   matRad_plotSOBP(ct, stf, cube1, cube2)
+%
+% input
+%   ct:             ct cube
+%   stf:            matRad steering information struct
+%   cube1:          dose cube 1
+%   cube2:          dose cube 2 of same size for comparison (optional)
+%
+% output
+%   figure
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2019 the matRad development team.
+%
+% This file is part of the matRad project. It is subject to the license
+% terms in the LICENSE file found in the top-level directory of this
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part
+% of the matRad project, including this file, may be copied, modified,
+% propagated, or distributed except according to the terms contained in the
+% LICENSE file.
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 if exist('cube2')
     checkSize = size(cube1) == size(cube2);
     if ~all(checkSize)
@@ -16,7 +43,6 @@ for i = 1:length(stf)
     
     alphaMid = (alphas(1:end-1) + alphas(2:end))/2;
     physMid = alphaMid * d12;
-    %physMidRel = physMid - min(physMid);
     values = cube1(ix);
     
     figure;
