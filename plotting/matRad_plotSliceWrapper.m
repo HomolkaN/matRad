507813a27b0e28120c83957e4500a282589ad830
homolka <n.homolka@dkfz.de>
2019-10-29 13:21:52 +0100

    Added a few more options to the plotSliceWrapper useful for subfigures

--- plotting/matRad_plotSliceWrapper.m
+++ plotting/matRad_plotSliceWrapper.m
@@ -1,5 +1,5 @@
-function [hCMap,hDose,hCt,hContour,hIsoDose] = matRad_plotSliceWrapper(axesHandle,ct,cst,cubeIdx,dose,plane,slice,thresh,alpha,contourColorMap,...
-                                                                          doseColorMap,doseWindow,doseIsoLevels,voiSelection,colorBarLabel,boolPlotLegend,varargin)
+function [hCMap,hDose,hCt,hContour,hIsoDose] = matRad_plotSliceWrapper(axesHandle,ct,cst,cubeIdx,dose,plane,slice,windowXY,thresh,alpha,contourColorMap,...
+    doseColorMap,doseWindow,doseIsoLevels,voiSelection,suppressCbar,colorBarLabel,boolPlotLegend,varargin)
 
 % matRad tool function to directly plot a complete slice of a ct with dose
 % including contours and isolines.
@@ -16,6 +16,7 @@ function [hCMap,hDose,hCt,hContour,hIsoDose] = matRad_plotSliceWrapper(axesHandl
 %   slice           slice in the selected plane of the 3D cube
 %
 % input (optional / empty)
+%   windowXY        overall plot window
 %   thresh          threshold for display of dose values
 %   alpha           alpha value for the dose overlay
 %   contourColorMap colormap for the VOI contours
@@ -25,11 +26,12 @@ function [hCMap,hDose,hCt,hContour,hIsoDose] = matRad_plotSliceWrapper(axesHandl
 %   voiSelection    logicals defining the current selection of contours
 %                   that should be plotted. Can be set to [] to plot
 %                   all non-ignored contours.
+%   suppressCbar    do not generate colorbar (useful when usage in subplots)
 %   colorBarLabel   string defining the yLabel of the colorBar
 %   boolPlotLegend  boolean if legend should be plottet or not
 %   varargin        additional input parameters that are passed on to
 %                   individual plotting functions (e.g. 'LineWidth',1.5)
-%   
+%
 %
 % output
 %   hCMap       handle to the colormap
@@ -40,18 +42,21 @@ function [hCMap,hDose,hCt,hContour,hIsoDose] = matRad_plotSliceWrapper(axesHandl
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Copyright 2015 the matRad development team. 
-% 
-% This file is part of the matRad project. It is subject to the license 
-% terms in the LICENSE file found in the top-level directory of this 
-% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
-% of the matRad project, including this file, may be copied, modified, 
-% propagated, or distributed except according to the terms contained in the 
+% Copyright 2015 the matRad development team.
+%
+% This file is part of the matRad project. It is subject to the license
+% terms in the LICENSE file found in the top-level directory of this
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part
+% of the matRad project, including this file, may be copied, modified,
+% propagated, or distributed except according to the terms contained in the
 % LICENSE file.
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Handle the argument list
+if ~exist('windowXY','var') || isempty(windowXY)
+    windowXY = [];
+end
 if ~exist('thresh','var') || isempty(thresh)
     thresh = [];
 end
@@ -59,37 +64,41 @@ if ~exist('alpha','var') || isempty(alpha)
     alpha = [];
 end
 if ~exist('contourColorMap','var') || isempty(contourColorMap)
-   contourColorMap = [];
+    contourColorMap = [];
 end
 if ~exist('doseColorMap','var') || isempty(doseColorMap)
-   doseColorMap = [];
+    doseColorMap = [];
 end
 if ~exist('doseWindow','var') || isempty(doseWindow)
-   doseWindow = [];
+    doseWindow = [];
 end
 if ~exist('doseIsoLevels','var') || isempty(doseIsoLevels)
-   doseIsoLevels = [];
+    doseIsoLevels = [];
 end
 
 if ~exist('voiSelection','var') || isempty(voiSelection)
-   voiSelection = [];
+    voiSelection = [];
+end
+
+if ~exist('suppressCbar','var') || isempty(suppressCbar)
+    suppressCbar = false;
 end
 
 if ~exist('colorBarLabel','var') || isempty(colorBarLabel)
-   colorBarLabel = [];
+    colorBarLabel = [];
 end
 
 if ~exist('boolPlotLegend','var') || isempty(boolPlotLegend)
-   boolPlotLegend = false;
+    boolPlotLegend = false;
 end
 
 if ~exist('cst','var') || isempty(cst)
-   cst = [];
+    cst = [];
 end
 
 set(axesHandle,'YDir','Reverse');
 % plot ct slice
-hCt = matRad_plotCtSlice(axesHandle,ct.cubeHU,cubeIdx,plane,slice); 
+hCt = matRad_plotCtSlice(axesHandle,ct.cubeHU,cubeIdx,plane,slice);
 hold on;
 
 % plot dose
@@ -106,21 +115,21 @@ end
 %plot VOI contours
 if  ~isempty(cst)
     hContour = matRad_plotVoiContourSlice(axesHandle,cst,ct.cubeHU,cubeIdx,voiSelection,plane,slice,contourColorMap,varargin{:});
-
-if boolPlotLegend
-   visibleOnSlice = (~cellfun(@isempty,hContour));
-   ixLegend = find(voiSelection);
-   hContourTmp    = cellfun(@(X) X(1),hContour(visibleOnSlice),'UniformOutput',false);
-   if ~isempty(voiSelection)
-       hLegend        =  legend(axesHandle,[hContourTmp{:}],[cst(ixLegend(visibleOnSlice),2)],'AutoUpdate','off');
-   else
-       hLegend        =  legend(axesHandle,[hContourTmp{:}],[cst(visibleOnSlice,2)],'AutoUpdate','off');
-   end
-   set(hLegend,'Box','Off');
-   set(hLegend,'TextColor',[1 1 1]);
-   set(hLegend,'FontSize',12);
-
-end
+    
+    if boolPlotLegend
+        visibleOnSlice = (~cellfun(@isempty,hContour));
+        ixLegend = find(voiSelection);
+        hContourTmp    = cellfun(@(X) X(1),hContour(visibleOnSlice),'UniformOutput',false);
+        if ~isempty(voiSelection)
+            hLegend        =  legend(axesHandle,[hContourTmp{:}],[cst(ixLegend(visibleOnSlice),2)],'AutoUpdate','off');
+        else
+            hLegend        =  legend(axesHandle,[hContourTmp{:}],[cst(visibleOnSlice,2)],'AutoUpdate','off');
+        end
+        set(hLegend,'Box','Off');
+        set(hLegend,'TextColor',[1 1 1]);
+        set(hLegend,'FontSize',12);
+        
+    end
 else
     hContour = [];
 end
@@ -129,28 +138,37 @@ axis(axesHandle,'tight');
 set(axesHandle,'xtick',[],'ytick',[]);
 colormap(axesHandle,doseColorMap);
 
+if ~isempty(windowXY)
+    if ~(windowXY{2}(2)-windowXY{2}(1) == windowXY{1}(2)-windowXY{1}(1))
+    warning('XY window is not a square, image will be distorted.')
+    end
+    xlim(windowXY{1})
+    ylim(windowXY{2})
+end
+
 matRad_plotAxisLabels(axesHandle,ct,plane,slice,[])
 
 % set axis ratio
 
 ratios = [1/ct.resolution.x 1/ct.resolution.y 1/ct.resolution.z];
-   
+
 set(axesHandle,'DataAspectRatioMode','manual');
-if plane == 1 
-      res = [ratios(3) ratios(2)]./max([ratios(3) ratios(2)]);  
-      set(axesHandle,'DataAspectRatio',[res 1])
+if plane == 1
+    res = [ratios(3) ratios(2)]./max([ratios(3) ratios(2)]);
+    set(axesHandle,'DataAspectRatio',[res 1])
 elseif plane == 2 % sagittal plane
-      res = [ratios(3) ratios(1)]./max([ratios(3) ratios(1)]);  
-      set(axesHandle,'DataAspectRatio',[res 1]) 
+    res = [ratios(3) ratios(1)]./max([ratios(3) ratios(1)]);
+    set(axesHandle,'DataAspectRatio',[res 1])
 elseif  plane == 3 % Axial plane
-      res = [ratios(2) ratios(1)]./max([ratios(2) ratios(1)]);  
-      set(axesHandle,'DataAspectRatio',[res 1])
+    res = [ratios(2) ratios(1)]./max([ratios(2) ratios(1)]);
+    set(axesHandle,'DataAspectRatio',[res 1])
 end
 
-hCMap = matRad_plotColorbar(axesHandle,doseColorMap,doseWindow,'Location','EastOutside');
-if ~isempty(colorBarLabel)
-    set(get(hCMap,'YLabel'),'String', colorBarLabel,'FontSize',14);
+if ~suppressCbar
+    hCMap = matRad_plotColorbar(axesHandle,doseColorMap,doseWindow,'Location','EastOutside');
+    if ~isempty(colorBarLabel)
+        set(get(hCMap,'YLabel'),'String', colorBarLabel,'FontSize',14);
+    end
 end
-
 end
 
