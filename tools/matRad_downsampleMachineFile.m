4e61b59a9f6c14a15515fb00358c5cc6a0f2d783
Noa Homolka <n.homolka@dkfz.de>
2023-05-31 13:53:42 +0200

    Update matRad_downsampleMachineFile.m
    refined downsampling with a fine grid at peak and a smooth gradient to the rest of the base data (maximal agreement)

--- tools/matRad_downsampleMachineFile.m
+++ tools/matRad_downsampleMachineFile.m
@@ -24,7 +24,8 @@ function machine = matRad_downsampleMachineFile(machine)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 downsampleFactor = 0.2; % Specifies percentage of grid spacing, the smaller the broader the grid
-peakArea = 0.02; % Specifies percentage of peak area
+lowerPeakArea = 0.05; % Specifies percentage of peak area
+upperPeakArea = 0.04; % Specifies percentage of peak area
 
 for energyIx = 1:numel(machine.data)
 
@@ -34,16 +35,33 @@ for energyIx = 1:numel(machine.data)
     newGrid = round(max(depths)/numOfSamplePoints,2);
 
     % Isolate peak area to use original grid
-    [~,leftPos] = min(abs(machine.data(energyIx).depths-machine.data(energyIx).peakPos*(1-peakArea)));
-    [~,rightPos] = min(abs(machine.data(energyIx).depths-machine.data(energyIx).peakPos*(1+peakArea)));
+    [~,leftPos] = min(abs(machine.data(energyIx).depths-machine.data(energyIx).peakPos*(1-lowerPeakArea)));
+    [~,rightPos] = min(abs(machine.data(energyIx).depths-machine.data(energyIx).peakPos*(1+upperPeakArea)));
 
     % Generate new depths vector
-    newDepths = matRad_interp1(depths,depths,min(depths):newGrid:max(depths));
+    newDepths = matRad_interp1(depths,depths,min(depths):newGrid:1.3*machine.data(energyIx).peakPos,'extrap');
 
-    % Use original grid vor the peak area
+    % Only use new grid for everything that's not peak
     newDepths(newDepths>depths(leftPos) & newDepths<depths(rightPos)) = [];
+    % Use original grid for the peak area
     newDepths = [newDepths; depths(leftPos:rightPos)];
     newDepths = sort(newDepths);
+    newDepths = smooth(newDepths,20);
+    newDepths = round(newDepths,5);
+%     newDepths = 0:0.1:1.3*machine.data(energyIx).peakPos;
+%     newDepths = newDepths + 0.05;
+
+
+    % Plot
+    if energyIx == 30
+        edges = 0:2:max(newDepths);
+        figure, yyaxis left, histogram(depths,edges), hold on, histogram(newDepths,edges)
+        xlim([0 max(newDepths)])
+        xlabel('Depth (mm)')
+        ylabel('counts')
+        yyaxis right, plot(depths,machine.data(energyIx).Z,'LineWidth',2), hold on, plot(newDepths,matRad_interp1(depths,machine.data(energyIx).Z,newDepths),'LineWidth',2,'LineStyle','--');
+        ylabel('Dose Z (a.u.)')
+    end
 
     % Loop through all fields and apply downsampling
     fnames = fieldnames(machine.data(energyIx));
