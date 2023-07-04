4e24eea5351676b7534199d50ef5b5d28602e7cd
Noa Homolka <n.homolka@dkfz.de>
2023-07-04 18:22:11 +0200

    clean up MCemittanceBaseData a bit

--- matRad_MCemittanceBaseData.m
+++ matRad_MCemittanceBaseData.m
@@ -230,6 +230,8 @@ classdef matRad_MCemittanceBaseData
         function mcDataEnergy = fitPhaseSpaceForEnergy(obj,energyIx)
             % function to calculate mean energy and energy spread used by Monte Carlo for given energy
             
+            matRad_cfg = MatRad_Config.instance();
+
             % Considers air distance from nozzle to phantom surface used in the machine data.
             % 0 means fitted to vacuum simulations with surface at isocenter
             if ~isfield(obj.machine.meta, 'fitAirOffset')
@@ -242,7 +244,7 @@ classdef matRad_MCemittanceBaseData
             airOffsetCorrection = 0.0011 * (fitAirOffset);
                         
             % Save given energy as nominal energy
-            mcDataEnergy.NominalEnergy = ones(1, size(obj.machine.data(1).initFocus.dist,1)) * obj.machine.data(energyIx).energy;
+            mcDataEnergy.NominalEnergy = obj.machine.data(energyIx).energy;
             
             % Interpolate depth dose to fine grid
             depths_interp = linspace(0,obj.machine.data(energyIx).depths(end),numel(obj.machine.data(energyIx).depths) * 100);
@@ -258,42 +260,39 @@ classdef matRad_MCemittanceBaseData
             % Calcualte mean energy used my mcSquare with a formula fitted to TOPAS data
             switch obj.machine.meta.radiationMode
                 case 'protons'
-                    %%% Approximate mean energy
-                    % This rangeEnergy relationship was created from a series of Monte Carlo simulations where the nominal
-                    % energy was equal to the mean energy (P. Meder, 2021)
-%                     rangeEnergyFit = @(x) 5.762374661332111e-20 * x.^9 - 9.645413625310569e-17 * x.^8 + 7.073049219034644e-14 * x.^7 ...
-%                         - 2.992344292008054e-11 * x.^6 + 8.104111934547256e-09 * x.^5 - 1.477860913846939e-06 * x.^4 ...
-%                         + 1.873625800704108e-04 * x.^3 - 1.739424343114980e-02 * x.^2 + 1.743224692623838e+00 * x ...
-%                         + 1.827112816899668e+01;
-                    
+                    %%% Approximate mean energy                   
                     % This rangeEnergy relationship was created analogously to the helium and carbon relationship below
                     % Fitted to data from "Update to ESTAR, PSTAR, and ASTAR Databases" - ICRU Report 90, 2014
                     rangeEnergyFit = @(x) 7.62* x.^0.5842 + 3.063;
 
                     %%% Calculate energy spread from FWHM
-                    % Calculate FWHM of bragg peak
-                    d50_r = interp1(dose_interp(maxDoseIdx:end), depths_interp(maxDoseIdx:end), 0.5 * maxDose);
-                    % Calculate left d50 if the plateau is lower than 50% of the max Dose
+                    % Calculate FWHM of bragg peak if the plateau is lower than 50% of the max Dose
                     if (dose_interp(1) < 0.5 * maxDose)
+                        % Calculate left and right flank, where dose falls to 50%
+                        d50_r = interp1(dose_interp(maxDoseIdx:end), depths_interp(maxDoseIdx:end), 0.5 * maxDose);
                         d50_l = interp1(dose_interp(1:maxDoseIdx), depths_interp(1:maxDoseIdx), 0.5 * maxDose);
+                        
+                        % Combine for FWHM
                         FWHM = d50_r - d50_l;
                     else
                         % if width left of peak cannot be determined use r80 as width
-                        % d50_l = newDepths(maxI);
                         FWHM = r80;
                         obj.problemSigma = true;
+                        matRad_cfg.warning('Could not find FWHM, using r80 as approximation');
                     end
 
                     % Calculate energy straggling using formulae deducted from paper
                     % "An analytical approximation of the Bragg curve for therapeutic proton beams" by T. Bortfeld et al.
                     totalSigmaSq = (FWHM / 6.14)^2;
 
+                    % Inverse fit to rangeEnergy relationship?
                     totalSpreadSq = @(x) 2.713311945114106e-20 * x^9 - 4.267890251195303e-17 * x^8 + 2.879118523083018e-14 * x^7 ...
                         - 1.084418008735459e-11 * x^6 + 2.491796224784373e-09 * x^5 - 3.591462823163767e-07 * x^4 ...
                         + 3.232810400304542e-05 * x^3 - 1.584729282376364e-03 * x^2 + 5.228413840446568e-02 * x ...
                         - 6.547482267336220e-01;
                     
-                    % Use formula deducted from Bragg Kleeman rule to calcuate energy straggling given the total sigma and the range straggling
+                    % Use formula deducted from Bragg Kleeman rule to calcuate energy straggling given the total sigma
+                    % and the range straggling (Bortfeld 1997, Eq.19)
                     energySpread = (totalSigmaSq - totalSpreadSq(r80)) / (0.022^2 * 1.77^2 * rangeEnergyFit(r80)^(2*1.77-2));
                     energySpread(energySpread < 0) = 0;
                     energySpread = sqrt(energySpread);
@@ -308,6 +307,8 @@ classdef matRad_MCemittanceBaseData
                     %%% Energy spread
                     % Reading in a potential given energyspread could go here directly. How would you parse the energyspread
                     % into the function? Through a field in the machine?
+                    % TODO this could be done analogously to protons, adjusting the alpha and p parameter from a E0(R0)
+                    % graph (inverse of the fit above)
                     energySpread = obj.defaultRelativeEnergySpread;
                 case 'helium'
                     %%% Fit to Range-Energy relationship
