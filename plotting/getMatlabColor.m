5425e92d96e8f1c3b2c04d09ffc46876f5c31468
Noa Homolka <n.homolka@dkfz.de>
2024-01-04 16:49:28 +0100

    Update getMatlabColor.m

--- plotting/getMatlabColor.m
+++ plotting/getMatlabColor.m
@@ -1,19 +1,22 @@
 function rgb = getMatlabColor(color)
 
-if strcmp(color,'blue')
-    rgb = [0, 0.4470, 0.7410];
-elseif strcmp(color,'orange')
-    rgb = [0.8500, 0.3250, 0.0980];
-elseif strcmp(color,'yellow')
-    rgb = [0.9290, 0.6940, 0.1250];
-elseif strcmp(color,'purple')
-    rgb = [0.4940, 0.1840, 0.5560];
-elseif strcmp(color,'green')
-    rgb = [0.4660, 0.6740, 0.1880];
-elseif strcmp(color,'cyan')
-    rgb = [0.3010, 0.7450, 0.9330];
-elseif strcmp(color,'red')
-    rgb = [0.6350, 0.0780, 0.1840];
+switch color
+    case 'blue'
+        rgb = [0, 0.4470, 0.7410];
+    case 'orange'
+        rgb = [0.8500, 0.3250, 0.0980];
+    case 'yellow'
+        rgb = [0.9290, 0.6940, 0.1250];
+    case 'purple'
+        rgb = [0.4940, 0.1840, 0.5560];
+    case 'green'
+        rgb = [0.4660, 0.6740, 0.1880];
+    case 'cyan'
+        rgb = [0.3010, 0.7450, 0.9330];
+    case 'red'
+        rgb = [0.6350, 0.0780, 0.1840];
+    case 'magenta'
+        rgb = [1, 0, 1];
 end
 
 end
