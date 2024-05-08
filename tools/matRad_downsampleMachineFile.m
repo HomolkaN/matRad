88372dcf374a274e30cf7db58ea3fc34cfd6cfb3
Noa Homolka <n.homolka@dkfz.de>
2024-05-08 17:03:15 +0200

    create new protons_generic_newFitted downsampled machine file (spline)

--- tools/matRad_downsampleMachineFile.m
+++ tools/matRad_downsampleMachineFile.m
@@ -27,14 +27,14 @@ if nargin < 2
     energies = 1:length(machine.data);
 end
 if nargin < 3
-    fineSamplingFirst = true;
+    fineSamplingFirst = false;
 end
 
-downsampleFactor = 0.2; % Specifies percentage of grid spacing, the smaller the broader the grid
-lowerPeakArea = 0.05; % Specifies percentage of peak area
-upperPeakArea = 0.04; % Specifies percentage of peak area
-depthCutOff = 1.2;
-% depthCutOff = 0;
+downsampleFactor = 0.1; % Specifies percentage of grid spacing, the smaller the broader the grid
+lowerPeakArea = 0.08; % Specifies percentage of peak area
+upperPeakArea = 0.06; % Specifies percentage of peak area
+% depthCutOff = 1.2;
+depthCutOff = 0;
 % cutOff = 0.005; %cutOff data at 0.001*max(Z)
 
 for energyIx = energies%numel(machine.data)
@@ -57,15 +57,15 @@ for energyIx = energies%numel(machine.data)
     end
 
     % Define updated grid
-    numOfSamplePoints = downsampleFactor*numel(machine.data(energyIx).depths);
-    depths = machine.data(energyIx).depths;
-    newGrid = round(max(depths)/numOfSamplePoints,2);
+    numOfSamplePoints   = downsampleFactor * numel(machine.data(energyIx).depths);
+    depths              = machine.data(energyIx).depths;
+    newGrid             = round(max(depths)/numOfSamplePoints,2);
 
     % CutOff data
-%     cutOffPoint = find(machine.data(energyIx).Z<cutOff*max(machine.data(energyIx).Z));
-%     if ~isempty(cutOffPoint)
-%        cutOffDepth = depths(cutOffPoint(1));
-%     end
+    %     cutOffPoint = find(machine.data(energyIx).Z<cutOff*max(machine.data(energyIx).Z));
+    %     if ~isempty(cutOffPoint)
+    %        cutOffDepth = depths(cutOffPoint(1));
+    %     end
 
     % Isolate peak area to use original grid
     [~,leftPos] = min(abs(machine.data(energyIx).depths-machine.data(energyIx).peakPos*(1-lowerPeakArea)));
@@ -103,18 +103,40 @@ for energyIx = energies%numel(machine.data)
             subFnames = fieldnames(machine.data(energyIx).(fnames{fieldIx}));
             for subFieldIx = 1:length(subFnames)
                 if ~isstruct(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})) && ~all(size(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}))<depthSize)
-                        machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}) = matRad_interp1(depths,machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}),newDepths,'extrap');
+                    % % Calculate moving average
+                    % depthDists = [min(newDepths) diff(newDepths')/2 max(newDepths)];
+                    % for i = 1:length(newDepths)
+                    %     toAverage(i,:) = (depths>=(newDepths(i)-depthDists(i)) & depths<(newDepths(i)+depthDists(i+1)));
+                    %     newCurrData(i) = mean(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(toAverage(i,:)));
+                    % end
+                    % if any(sum(toAverage,1)>1) || any(isnan(newCurrData))
+                    %     warning(sprintf('Problem with depth interpolation with energy %i',energyIx))
+                    % end
 
-                        % Cutoff with depth
-                        if depthCutOff>0
-                            machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(newDepths>machine.data(energyIx).peakPos*depthCutOff) = [];
-                        end
+                    % Spline interpolation
+                    newCurrData = interp1(depths,machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}),newDepths,'makima');
+                    % Adjust first value as mean of the first half new depth
+                    newCurrData(1) = mean(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(depths<=newDepths(1)+diff(newDepths(1:2)/2)));
+
+                    % Write new interpolated data
+                    machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}) = newCurrData;
+
+                    % Cutoff with depth
+                    if depthCutOff>0
+                        machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(newDepths>machine.data(energyIx).peakPos*depthCutOff) = [];
+                    end
                 end
             end
 
         elseif ~all(size(machine.data(energyIx).(fnames{fieldIx}))<depthSize)
-            machine.data(energyIx).(fnames{fieldIx}) = matRad_interp1(depths,machine.data(energyIx).(fnames{fieldIx}),newDepths,'extrap');
-            
+            % Spline interpolation
+            newCurrData = interp1(depths,machine.data(energyIx).(fnames{fieldIx}),newDepths,'makima');
+            % Adjust first value as mean of the first half new depth
+            newCurrData(1) = mean(machine.data(energyIx).(fnames{fieldIx})(depths<=newDepths(1)+diff(newDepths(1:2)/2)));
+
+            % Write new interpolated data
+            machine.data(energyIx).(fnames{fieldIx}) = newCurrData;
+
             % Cutoff with depth
             if depthCutOff>0
                 machine.data(energyIx).(fnames{fieldIx})(newDepths>machine.data(energyIx).peakPos*depthCutOff) = [];
@@ -122,7 +144,7 @@ for energyIx = energies%numel(machine.data)
 
         end
     end
-    
+
     clear depths newDepths
 
 end
