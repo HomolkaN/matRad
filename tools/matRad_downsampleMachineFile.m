9b5f670e8e2c9af320226ea515d0031e93117c18
Noa Homolka <n.homolka@dkfz.de>
2024-05-09 17:43:16 +0200

    Update matRad_downsampleMachineFile.m
    update downsampling function to only downsample not empty energies (for semi-done base data sets)

--- tools/matRad_downsampleMachineFile.m
+++ tools/matRad_downsampleMachineFile.m
@@ -39,114 +39,115 @@ depthCutOff = 0;
 
 for energyIx = energies%numel(machine.data)
 
-    if fineSamplingFirst
-        newDepths = min(machine.data(energyIx).depths):0.1:max(machine.data(energyIx).depths);
-        if isstruct(machine.data(energyIx).Z)
-            machine.data(energyIx).Z.profileORG = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).Z.profileORG,newDepths);
-            machine.data(energyIx).Z.profileAPM = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).Z.profileAPM,newDepths);
-        else
-            machine.data(energyIx).Z = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).Z,newDepths);
+    if ~(machine.data(energyIx).energy==0)
+        if fineSamplingFirst
+            newDepths = min(machine.data(energyIx).depths):0.1:max(machine.data(energyIx).depths);
+            if isstruct(machine.data(energyIx).Z)
+                machine.data(energyIx).Z.profileORG = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).Z.profileORG,newDepths);
+                machine.data(energyIx).Z.profileAPM = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).Z.profileAPM,newDepths);
+            else
+                machine.data(energyIx).Z = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).Z,newDepths);
+            end
+            machine.data(energyIx).weight = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).weight,newDepths);
+            machine.data(energyIx).sigma1 = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).sigma1,newDepths);
+            machine.data(energyIx).sigma2 = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).sigma2,newDepths);
+            if isfield(machine.data(energyIx),'LET')
+                machine.data(energyIx).LET = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).LET,newDepths);
+            end
+            machine.data(energyIx).depths = newDepths';
         end
-        machine.data(energyIx).weight = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).weight,newDepths);
-        machine.data(energyIx).sigma1 = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).sigma1,newDepths);
-        machine.data(energyIx).sigma2 = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).sigma2,newDepths);
-        if isfield(machine.data(energyIx),'LET')
-            machine.data(energyIx).LET = matRad_interp1(machine.data(energyIx).depths,machine.data(energyIx).LET,newDepths);
-        end
-        machine.data(energyIx).depths = newDepths';
-    end
 
-    % Define updated grid
-    numOfSamplePoints   = downsampleFactor * numel(machine.data(energyIx).depths);
-    depths              = machine.data(energyIx).depths;
-    newGrid             = round(max(depths)/numOfSamplePoints,2);
+        % Define updated grid
+        numOfSamplePoints   = downsampleFactor * numel(machine.data(energyIx).depths);
+        depths              = machine.data(energyIx).depths;
+        newGrid             = round(max(depths)/numOfSamplePoints,2);
 
-    % CutOff data
-    %     cutOffPoint = find(machine.data(energyIx).Z<cutOff*max(machine.data(energyIx).Z));
-    %     if ~isempty(cutOffPoint)
-    %        cutOffDepth = depths(cutOffPoint(1));
-    %     end
+        % CutOff data
+        %     cutOffPoint = find(machine.data(energyIx).Z<cutOff*max(machine.data(energyIx).Z));
+        %     if ~isempty(cutOffPoint)
+        %        cutOffDepth = depths(cutOffPoint(1));
+        %     end
 
-    % Isolate peak area to use original grid
-    [~,leftPos] = min(abs(machine.data(energyIx).depths-machine.data(energyIx).peakPos*(1-lowerPeakArea)));
-    [~,rightPos] = min(abs(machine.data(energyIx).depths-machine.data(energyIx).peakPos*(1+upperPeakArea)));
+        % Isolate peak area to use original grid
+        [~,leftPos] = min(abs(machine.data(energyIx).depths-machine.data(energyIx).peakPos*(1-lowerPeakArea)));
+        [~,rightPos] = min(abs(machine.data(energyIx).depths-machine.data(energyIx).peakPos*(1+upperPeakArea)));
 
-    % Generate new depths vector
-    newDepths = matRad_interp1(depths,depths,min(depths):newGrid:max(depths));
+        % Generate new depths vector
+        newDepths = matRad_interp1(depths,depths,min(depths):newGrid:max(depths));
 
-    % Only use new grid for everything that's not peak
-    newDepths(newDepths>depths(leftPos) & newDepths<depths(rightPos)) = [];
-    % Use original grid for the peak area
-    newDepths = [newDepths; depths(leftPos:rightPos)];
-    newDepths = sort(newDepths);
-    newDepths = smooth(newDepths,20);
-    newDepths = round(newDepths,5);
+        % Only use new grid for everything that's not peak
+        newDepths(newDepths>depths(leftPos) & newDepths<depths(rightPos)) = [];
+        % Use original grid for the peak area
+        newDepths = [newDepths; depths(leftPos:rightPos)];
+        newDepths = sort(newDepths);
+        newDepths = smooth(newDepths,20);
+        newDepths = round(newDepths,5);
 
-    % Plot
-    % if energyIx == 10
-    %     plotPoints = newDepths<machine.data(energyIx).peakPos*depthCutOff;
-    %     edges = 0:2:max(newDepths(plotPoints));
-    %     figure, yyaxis left, histogram(depths,edges), hold on, histogram(newDepths(plotPoints),edges)
-    %     xlim([0 max(newDepths(plotPoints))])
-    %     xlabel('Depth (mm)')
-    %     ylabel('counts')
-    %     yyaxis right, plot(depths,machine.data(energyIx).Z,'LineWidth',2), hold on, plot(newDepths(plotPoints),matRad_interp1(depths,machine.data(energyIx).Z,newDepths(plotPoints)),'LineWidth',2,'LineStyle','--');
-    %     ylabel('Dose Z (a.u.)')
-    % end
+        % Plot
+        % if energyIx == 10
+        %     plotPoints = newDepths<machine.data(energyIx).peakPos*depthCutOff;
+        %     edges = 0:2:max(newDepths(plotPoints));
+        %     figure, yyaxis left, histogram(depths,edges), hold on, histogram(newDepths(plotPoints),edges)
+        %     xlim([0 max(newDepths(plotPoints))])
+        %     xlabel('Depth (mm)')
+        %     ylabel('counts')
+        %     yyaxis right, plot(depths,machine.data(energyIx).Z,'LineWidth',2), hold on, plot(newDepths(plotPoints),matRad_interp1(depths,machine.data(energyIx).Z,newDepths(plotPoints)),'LineWidth',2,'LineStyle','--');
+        %     ylabel('Dose Z (a.u.)')
+        % end
 
 
-    % Loop through all fields and apply downsampling
-    depthSize = numel(depths);
-    fnames = fieldnames(machine.data(energyIx));
-    for fieldIx = 1:length(fnames)
-        if isstruct(machine.data(energyIx).(fnames{fieldIx}))
-            subFnames = fieldnames(machine.data(energyIx).(fnames{fieldIx}));
-            for subFieldIx = 1:length(subFnames)
-                if ~isstruct(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})) && ~all(size(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}))<depthSize)
-                    % % Calculate moving average
-                    % depthDists = [min(newDepths) diff(newDepths')/2 max(newDepths)];
-                    % for i = 1:length(newDepths)
-                    %     toAverage(i,:) = (depths>=(newDepths(i)-depthDists(i)) & depths<(newDepths(i)+depthDists(i+1)));
-                    %     newCurrData(i) = mean(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(toAverage(i,:)));
-                    % end
-                    % if any(sum(toAverage,1)>1) || any(isnan(newCurrData))
-                    %     warning(sprintf('Problem with depth interpolation with energy %i',energyIx))
-                    % end
+        % Loop through all fields and apply downsampling
+        depthSize = numel(depths);
+        fnames = fieldnames(machine.data(energyIx));
+        for fieldIx = 1:length(fnames)
+            if isstruct(machine.data(energyIx).(fnames{fieldIx}))
+                subFnames = fieldnames(machine.data(energyIx).(fnames{fieldIx}));
+                for subFieldIx = 1:length(subFnames)
+                    if ~isstruct(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})) && ~all(size(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}))<depthSize)
+                        % % Calculate moving average
+                        % depthDists = [min(newDepths) diff(newDepths')/2 max(newDepths)];
+                        % for i = 1:length(newDepths)
+                        %     toAverage(i,:) = (depths>=(newDepths(i)-depthDists(i)) & depths<(newDepths(i)+depthDists(i+1)));
+                        %     newCurrData(i) = mean(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(toAverage(i,:)));
+                        % end
+                        % if any(sum(toAverage,1)>1) || any(isnan(newCurrData))
+                        %     warning(sprintf('Problem with depth interpolation with energy %i',energyIx))
+                        % end
 
-                    % Spline interpolation
-                    newCurrData = interp1(depths,machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}),newDepths,'makima');
-                    % Adjust first value as mean of the first half new depth
-                    newCurrData(1) = mean(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(depths<=newDepths(1)+diff(newDepths(1:2)/2)));
+                        % Spline interpolation
+                        newCurrData = interp1(depths,machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}),newDepths,'makima');
+                        % Adjust first value as mean of the first half new depth
+                        newCurrData(1) = mean(machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(depths<=newDepths(1)+diff(newDepths(1:2)/2)));
 
-                    % Write new interpolated data
-                    machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}) = newCurrData;
+                        % Write new interpolated data
+                        machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx}) = newCurrData;
 
-                    % Cutoff with depth
-                    if depthCutOff>0
-                        machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(newDepths>machine.data(energyIx).peakPos*depthCutOff) = [];
+                        % Cutoff with depth
+                        if depthCutOff>0
+                            machine.data(energyIx).(fnames{fieldIx}).(subFnames{subFieldIx})(newDepths>machine.data(energyIx).peakPos*depthCutOff) = [];
+                        end
                     end
                 end
+
+            elseif ~all(size(machine.data(energyIx).(fnames{fieldIx}))<depthSize)
+                % Spline interpolation
+                newCurrData = interp1(depths,machine.data(energyIx).(fnames{fieldIx}),newDepths,'makima');
+                % Adjust first value as mean of the first half new depth
+                newCurrData(1) = mean(machine.data(energyIx).(fnames{fieldIx})(depths<=newDepths(1)+diff(newDepths(1:2)/2)));
+
+                % Write new interpolated data
+                machine.data(energyIx).(fnames{fieldIx}) = newCurrData;
+
+                % Cutoff with depth
+                if depthCutOff>0
+                    machine.data(energyIx).(fnames{fieldIx})(newDepths>machine.data(energyIx).peakPos*depthCutOff) = [];
+                end
+
             end
-
-        elseif ~all(size(machine.data(energyIx).(fnames{fieldIx}))<depthSize)
-            % Spline interpolation
-            newCurrData = interp1(depths,machine.data(energyIx).(fnames{fieldIx}),newDepths,'makima');
-            % Adjust first value as mean of the first half new depth
-            newCurrData(1) = mean(machine.data(energyIx).(fnames{fieldIx})(depths<=newDepths(1)+diff(newDepths(1:2)/2)));
-
-            % Write new interpolated data
-            machine.data(energyIx).(fnames{fieldIx}) = newCurrData;
-
-            % Cutoff with depth
-            if depthCutOff>0
-                machine.data(energyIx).(fnames{fieldIx})(newDepths>machine.data(energyIx).peakPos*depthCutOff) = [];
-            end
-
         end
+
+        clear depths newDepths
     end
-
-    clear depths newDepths
-
 end
 
 end
